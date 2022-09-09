bf7e52ae113815587dcae06ad6fcbb64b143faca
Renata19 <renatalsa@mail.ru>
2022-09-09 23:21:21 +0700

    11

---
+++ task11/Program.cs
@@ -0,0 +1,28 @@
+﻿    Console.WriteLine("Введите число 1");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Введите число 2");
+    int number2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+
+  if( number1 % number2 == 0)
+{
+    Console.WriteLine(" деление удалось");
+
+}
+else
+{
+    Console.WriteLine(number1 % number2);
+
+
+}
+
+
+
+
+
+
