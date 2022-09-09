77b9c0eed04e1f575fca7566a1dd0b54da322e0b
Renata19 <renatalsa@mail.ru>
2022-09-09 22:01:50 +0700

    10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Random rand = new Random();
+int number = rand.Next(100, 1000);
+
+Console.WriteLine($"Сгенерировлось случайное число {number}");
+
+int digit3 = number % 10;
+int digit1 = number / 100;
+
+    Console.WriteLine($"Полученное число {digit1}{digit3}");
